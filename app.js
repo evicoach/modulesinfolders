35ab1e9e2bf2c6bfb75f0cca5dc8a50dec17d8b7
Evi Owhotemu <evicoach@gmail.com>
2020-01-14 15:00:21 +0100

    Working effectively

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 
-const dirPath = '/media/evi/UBUNTU 19_1/Tutorials/NodeJS';
+// get the first argument pass to the app
+// the folder location
+const dirPath = process.argv.slice(2)[0];
 
-const getHyphen = (file) => {
+let dirName = '';
+
+const getDirectoryName = (file) => {
     let hiphen = file.indexOf('-0');
     if (hiphen < 0)
         hiphen = file.indexOf('-1');
@@ -12,24 +16,81 @@ const getHyphen = (file) => {
         hiphen = file.indexOf('-2');
 
 
-    return hiphen;
+    if (hiphen > 0)
+        dirName = path.join(dirPath, file.substring(0, hiphen));
+
+
+    return dirName; // eg /basics
 }
 
+const getDestination = (file) => {
+    return path.join(getDirectoryName(file), file);
+}
+
+//moves the $file to $dir2
+const moveFile = (file, dest) => {
+
+    //gets file name and adds it to dir2
+    // var f = path.basename(file);
+    // var dest = path.resolve(dir2, f);
+
+    fs.open(dest, (err, fd) => {
+        if (err) {
+            if (err.code === 'EEXIST') {
+                return;
+            }
+            fs.rename(file, dest, (err) => {
+                if (err) throw err;
+                else console.log('Successfully moved');
+            });
+        } else return console.log('Nothing was moved');
+    })
+};
+
+const createFolder = (directoryName) => {
+
+    // check if folder exist
+    // if not create it
+    return fs.open(directoryName, (err, fd) => {
+        if (err) {
+            if (err.code === 'EEXIST') {
+                return;
+            }
+            fs.mkdir(directoryName, (err) => {
+                if (err) {
+                    if (err.code === 'EEXIST') {
+                        return;
+                    }
+                    console.log('Error creating folder:', err);
+                    throw err;
+                }
+            })
+        } else return console.log('No folders were created')
+    })
+}
+
+// read and list all the files in this folder
 fs.readdir(dirPath, (err, file) => {
     if (err) {
-        return console.log('Unable to scan directory ' + err);
+        console.log('Unable to scan directory:', err);
     }
 
-    // listing all files using forEach
-    file.forEach(file => {
-        // for each file, let the dir name be
-        let hiphen = getHyphen(file);
+    // use one forEach loop to create all the needed directories
+    file.forEach((file) => {
+        let stat = fs.lstatSync(path.join(dirPath, file));
+        if (stat.isDirectory()) return; // return if there are no files
+        let obj = { name: file }
+        let obj2 = { ...obj }
+        let dirName = getDirectoryName(obj2.name);
 
-        // if found, ie, hiphen is greater than zero
-        if (hiphen > 0) {
-            let dirName = file.substring(0, hiphen);
-            console.log(dirName);
-
-        }
+        // create module folders
+        createFolder(dirName);
     });
+
+    // use another forEach loop to move all the files into it's module directory
+    file.forEach((file) => {
+        let stat = fs.lstatSync(path.join(dirPath, file));
+        if (stat.isDirectory()) return;
+        moveFile(path.join(dirPath, file), getDestination(file))
+    })
 });
